07abf178bfc058dfa746acce92d86ef73d8da545
Chris Papanicholas JR <cpapanicholas@gmail.com>
2023-09-21 22:23:05 -0500

    added splits and edited the spacing for 'const's

--- script.js
+++ script.js
@@ -12,12 +12,21 @@ const inclLower = confirm("Would you like your password to include lowercase cha
 const inclSymbol = confirm("Would you like your password to include symbols?");
 const inclNumber = confirm("Would you like your password to include numbers?");
 // Assignment code here
-const lowerCase = "abcdefghijklmnopqrstuvwxyz"
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const numbers = "123456789"
-const symbols = "!@#$%^&*()-_=+~`?<>"
+const lowerCase = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+const upperCase = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
+const numbers = "1 2 3 4 5 6 7 8 9"
+const symbols = "! @ # $ % ^ & * ( ) - _ = + ~ ` ? < >"
 
-if(isNaN(securityCodeLength) || securityCodeLength <=8) {}
+symbols.split (" ")
+numbers.split (" ")
+upperCase.split (" ")
+lowerCase.split (" ")
+
+
+
+if(isNaN(securityCodeLength) || securityCodeLength <=8) {
+  alert("Password needs to be at least 8 characters, please meet the password requirements");
+  } else
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -40,18 +49,3 @@ for (let i=0; i<length; i++) {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function askLowercase() {
-  if (confirm())
-
-
-
-
-
-
-prompt("do you want lowercase characters?")
-
-prompt("do you want uppercase characters?")
-
-prompt("do you want numbers?")
-
-prompt("do you want special characters?")
